e762281b44a07c2577a5e7b1dd858cbcd05a0815
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-08-17 04:12:28 +0530

    Update lockfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh -c 'cp .env.example .env && curl $DB_DOWNLOAD --output database/database.sqlite && vendor/bin/heroku-php-apache2 public/'
+web: sh -c 'cp .env.example .env && curl $DB_DOWNLOAD --output database/database.sqlite && composer install && vendor/bin/heroku-php-apache2 public/'
--- config/app.php
+++ config/app.php
@@ -194,7 +194,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
 
         Laravel\Scout\ScoutServiceProvider::class,
         TeamTNT\Scout\TNTSearchScoutServiceProvider::class,
