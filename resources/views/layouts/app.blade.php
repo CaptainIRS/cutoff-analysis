0f87cad4b7c5ef03c972d29db7392acbb9719cb5
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-09-11 07:45:20 +0530

    Reduce render blocking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,125 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
             color-scheme: var(--color-scheme);
             scroll-behavior: smooth;
         }
+
+        .lds-spinner {
+            display: inline-block;
+            position: fixed;
+            bottom: 40px;
+            right: 40px;
+            width: 40px;
+            height: 40px;
+        }
+
+        .lds-spinner.overlay-centered {
+            top: 50%;
+            left: 50%;
+            transform: translate(-40px, -40px);
+        }
+
+        .lds-spinner div {
+            transform-origin: 40px 40px;
+            animation: lds-spinner 1.2s linear infinite;
+        }
+
+        .lds-spinner div:after {
+            content: " ";
+            display: block;
+            position: absolute;
+            top: 10px;
+            left: 37px;
+            width: 6px;
+            height: 16px;
+            border-radius: 20%;
+            background: rgb(140, 140, 140);
+        }
+
+        .lds-spinner div:nth-child(1) {
+            transform: rotate(0deg);
+            animation-delay: -1.1s;
+        }
+
+        .lds-spinner div:nth-child(2) {
+            transform: rotate(30deg);
+            animation-delay: -1s;
+        }
+
+        .lds-spinner div:nth-child(3) {
+            transform: rotate(60deg);
+            animation-delay: -0.9s;
+        }
+
+        .lds-spinner div:nth-child(4) {
+            transform: rotate(90deg);
+            animation-delay: -0.8s;
+        }
+
+        .lds-spinner div:nth-child(5) {
+            transform: rotate(120deg);
+            animation-delay: -0.7s;
+        }
+
+        .lds-spinner div:nth-child(6) {
+            transform: rotate(150deg);
+            animation-delay: -0.6s;
+        }
+
+        .lds-spinner div:nth-child(7) {
+            transform: rotate(180deg);
+            animation-delay: -0.5s;
+        }
+
+        .lds-spinner div:nth-child(8) {
+            transform: rotate(210deg);
+            animation-delay: -0.4s;
+        }
+
+        .lds-spinner div:nth-child(9) {
+            transform: rotate(240deg);
+            animation-delay: -0.3s;
+        }
+
+        .lds-spinner div:nth-child(10) {
+            transform: rotate(270deg);
+            animation-delay: -0.2s;
+        }
+
+        .lds-spinner div:nth-child(11) {
+            transform: rotate(300deg);
+            animation-delay: -0.1s;
+        }
+
+        .lds-spinner div:nth-child(12) {
+            transform: rotate(330deg);
+            animation-delay: 0s;
+        }
+
+        @keyframes lds-spinner {
+            0% {
+                opacity: 1;
+            }
+
+            100% {
+                opacity: 0;
+            }
+        }
+
+        [x-cloak] {
+            opacity: 100 !important;
+        }
+
+        .overlay {
+            z-index: 2000;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+            background-color: var(--overlay-bg);
+            pointer-events: none;
+            transition: opacity 0.5s ease;
+        }
     </style>
     <script>
         if (localStorage.getItem('darkMode') === 'true') {
@@ -40,10 +159,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
             document.documentElement.style.setProperty('--color-scheme', 'light');
         }
     </script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @livewireStyles
-    @livewireScripts
-    @stack('scripts')
+
 </head>
 
 <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('app.ga4_tag') }}"></script>
@@ -242,5 +358,10 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
         </script>
     </div>
 </body>
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@livewireStyles
+@livewireScripts
+@stack('scripts')
 
 </html>
